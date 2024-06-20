bd4e4f61d640196bbbe39b7f503cf4c1167ffbb8
MilianCode <milian.code@gmail.com>
2024-06-20 19:36:12 +0200

    Implemented redirection

--- renovatio-api/src/main/java/org/example/renovatioapi/config/SecurityConfig.java
+++ renovatio-api/src/main/java/org/example/renovatioapi/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -26,11 +28,10 @@ public class SecurityConfig {
                                 .requestMatchers("/admin/panel/**").hasRole("ADMIN")
                                 .anyRequest().permitAll()
                 )
-                .formLogin(withDefaults())
-                .logout(logout ->
-                        logout.permitAll()
-                )
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler());
         return http.build();
     }
 
@@ -47,5 +48,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
     }
 }
